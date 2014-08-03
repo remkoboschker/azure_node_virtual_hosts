8a58279deb70e8c43f7bc392cb9a533dfc87ef43
Remko Boschker <remko@informatietuin.nl>
2014-08-03 13:44:38 +0200

    trust proxy in express en root domein

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express'),
     proxy = httpProxy.createProxyServer({}),
     atelierfemkeboschker = require('atelierfemkeboschker'),
     huisjeinegmond = http.createServer(function(req, res) {
+        console.log(req.originalUrl);
         if(req.originalUrl === "/"){
             res.writeHead(302, {
                 'Location' : '/index.html'
@@ -14,13 +15,15 @@ var express = require('express'),
             proxy.web(req, res, { target: 'https://huisjeinegmond.blob.core.windows.net' });
         }
     }),
+    
     app = express();
 
-    app.use(vhost('*.informatietuin.nl', atelierfemkeboschker))
+    app.enable('trust proxy')
+        .use(vhost('*.informatietuin.nl', atelierfemkeboschker))
+        .use(vhost('informatietuin.nl', atelierfemkeboschker))
         .use(vhost('*.huisjeinegmond.nl', huisjeinegmond))
-        
-    app.get('/', function (req, res) {
-        res.send('unknown virtual host');
-    });
-        
-    app.listen(80);
+        .use(vhost('huisjeinegmond.nl', huisjeinegmond))
+        .get('/', function (req, res) {
+            res.send(404, 'unknown virtual host');
+        })
+        .listen(80);
