c2997995679e08063d0ac7caa4d65f6412628c0a
Remko Boschker <remko@informatietuin.nl>
2014-08-03 14:45:09 +0200

    package main

--- index.js
+++ index.js
@@ -28,3 +28,9 @@ var express = require('express'),
             res.send(404, 'unknown virtual host');
         })
         .listen(port);
+//var http = require('http')
+//var port = process.env.PORT || 1337;
+//http.createServer(function(req, res) {
+//  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//  res.end('Hello World\n');
+//}).listen(port);
